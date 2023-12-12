6a85697a547c48124f9998fac4245c4ce3c6a5c0
parcorka <maksim.d.safonov@gmail.com>
2023-12-12 15:44:51 +0300

    Добавлены окошки с информацией
    добавлена информации о:
    ядрах процессора, имени текущего пользователя, имени компьютера, версии OS, материнской плате, BIOS

--- system info app/Form1.Designer.cs
+++ system info app/Form1.Designer.cs
@@ -28,57 +28,196 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox_processor = new System.Windows.Forms.TextBox();
+            this.label_processor = new System.Windows.Forms.Label();
+            this.textBox_graphics = new System.Windows.Forms.TextBox();
+            this.label_graphics = new System.Windows.Forms.Label();
+            this.textBox_cores = new System.Windows.Forms.TextBox();
+            this.label_cores = new System.Windows.Forms.Label();
+            this.textBox_user = new System.Windows.Forms.TextBox();
+            this.label_user = new System.Windows.Forms.Label();
+            this.textBox_os = new System.Windows.Forms.TextBox();
+            this.label_os = new System.Windows.Forms.Label();
+            this.label_pc = new System.Windows.Forms.Label();
+            this.textBox_pc = new System.Windows.Forms.TextBox();
+            this.textBox_motherboard = new System.Windows.Forms.TextBox();
+            this.label_motherboard = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
+            this.label_bios = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // textBox_processor
+            // 
+            this.textBox_processor.Location = new System.Drawing.Point(95, 21);
+            this.textBox_processor.Name = "textBox_processor";
+            this.textBox_processor.ReadOnly = true;
+            this.textBox_processor.Size = new System.Drawing.Size(258, 20);
+            this.textBox_processor.TabIndex = 0;
+            this.textBox_processor.TabStop = false;
+            // 
+            // label_processor
+            // 
+            this.label_processor.AutoSize = true;
+            this.label_processor.Location = new System.Drawing.Point(35, 24);
+            this.label_processor.Name = "label_processor";
+            this.label_processor.Size = new System.Drawing.Size(54, 13);
+            this.label_processor.TabIndex = 1;
+            this.label_processor.Text = "Processor";
+            // 
+            // textBox_graphics
+            // 
+            this.textBox_graphics.Location = new System.Drawing.Point(95, 82);
+            this.textBox_graphics.Name = "textBox_graphics";
+            this.textBox_graphics.ReadOnly = true;
+            this.textBox_graphics.Size = new System.Drawing.Size(258, 20);
+            this.textBox_graphics.TabIndex = 2;
+            this.textBox_graphics.TabStop = false;
+            // 
+            // label_graphics
+            // 
+            this.label_graphics.AutoSize = true;
+            this.label_graphics.Location = new System.Drawing.Point(20, 85);
+            this.label_graphics.Name = "label_graphics";
+            this.label_graphics.Size = new System.Drawing.Size(69, 13);
+            this.label_graphics.TabIndex = 3;
+            this.label_graphics.Text = "Graphic Card";
+            // 
+            // textBox_cores
+            // 
+            this.textBox_cores.Location = new System.Drawing.Point(95, 47);
+            this.textBox_cores.Name = "textBox_cores";
+            this.textBox_cores.ReadOnly = true;
+            this.textBox_cores.Size = new System.Drawing.Size(258, 20);
+            this.textBox_cores.TabIndex = 4;
+            this.textBox_cores.TabStop = false;
+            // 
+            // label_cores
+            // 
+            this.label_cores.AutoSize = true;
+            this.label_cores.Location = new System.Drawing.Point(3, 50);
+            this.label_cores.Name = "label_cores";
+            this.label_cores.Size = new System.Drawing.Size(86, 13);
+            this.label_cores.TabIndex = 5;
+            this.label_cores.Text = "number of cores:";
+            // 
+            // textBox_user
+            // 
+            this.textBox_user.Location = new System.Drawing.Point(428, 21);
+            this.textBox_user.Name = "textBox_user";
+            this.textBox_user.ReadOnly = true;
+            this.textBox_user.Size = new System.Drawing.Size(236, 20);
+            this.textBox_user.TabIndex = 6;
+            this.textBox_user.TabStop = false;
+            // 
+            // label_user
+            // 
+            this.label_user.AutoSize = true;
+            this.label_user.Location = new System.Drawing.Point(367, 24);
+            this.label_user.Name = "label_user";
+            this.label_user.Size = new System.Drawing.Size(55, 13);
+            this.label_user.TabIndex = 7;
+            this.label_user.Text = "Username";
+            // 
+            // textBox_os
+            // 
+            this.textBox_os.Location = new System.Drawing.Point(428, 50);
+            this.textBox_os.Name = "textBox_os";
+            this.textBox_os.ReadOnly = true;
+            this.textBox_os.Size = new System.Drawing.Size(236, 20);
+            this.textBox_os.TabIndex = 8;
+            this.textBox_os.TabStop = false;
+            // 
+            // label_os
+            // 
+            this.label_os.AutoSize = true;
+            this.label_os.Location = new System.Drawing.Point(362, 53);
+            this.label_os.Name = "label_os";
+            this.label_os.Size = new System.Drawing.Size(60, 13);
+            this.label_os.TabIndex = 9;
+            this.label_os.Text = "OS Version";
+            // 
+            // label_pc
+            // 
+            this.label_pc.AutoSize = true;
+            this.label_pc.Location = new System.Drawing.Point(372, 82);
+            this.label_pc.Name = "label_pc";
+            this.label_pc.Size = new System.Drawing.Size(50, 13);
+            this.label_pc.TabIndex = 10;
+            this.label_pc.Text = "PC name";
+            // 
+            // textBox_pc
+            // 
+            this.textBox_pc.Location = new System.Drawing.Point(428, 79);
+            this.textBox_pc.Name = "textBox_pc";
+            this.textBox_pc.ReadOnly = true;
+            this.textBox_pc.Size = new System.Drawing.Size(236, 20);
+            this.textBox_pc.TabIndex = 11;
+            this.textBox_pc.TabStop = false;
+            // 
+            // textBox_motherboard
+            // 
+            this.textBox_motherboard.Location = new System.Drawing.Point(95, 117);
+            this.textBox_motherboard.Name = "textBox_motherboard";
+            this.textBox_motherboard.ReadOnly = true;
+            this.textBox_motherboard.Size = new System.Drawing.Size(258, 20);
+            this.textBox_motherboard.TabIndex = 12;
+            this.textBox_motherboard.TabStop = false;
+            // 
+            // label_motherboard
+            // 
+            this.label_motherboard.AutoSize = true;
+            this.label_motherboard.Location = new System.Drawing.Point(22, 120);
+            this.label_motherboard.Name = "label_motherboard";
+            this.label_motherboard.Size = new System.Drawing.Size(67, 13);
+            this.label_motherboard.TabIndex = 13;
+            this.label_motherboard.Text = "Motherboard";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(84, 21);
+            this.textBox1.Location = new System.Drawing.Point(428, 108);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(258, 20);
-            this.textBox1.TabIndex = 0;
+            this.textBox1.Size = new System.Drawing.Size(236, 20);
+            this.textBox1.TabIndex = 14;
+            this.textBox1.TabStop = false;
             // 
-            // label1
+            // label_bios
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(24, 24);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(54, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Processor";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(84, 59);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(258, 20);
-            this.textBox2.TabIndex = 2;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 62);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(69, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Graphic Card";
+            this.label_bios.AutoSize = true;
+            this.label_bios.Location = new System.Drawing.Point(390, 111);
+            this.label_bios.Name = "label_bios";
+            this.label_bios.Size = new System.Drawing.Size(32, 13);
+            this.label_bios.TabIndex = 15;
+            this.label_bios.Text = "BIOS";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.label1);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(683, 349);
+            this.Controls.Add(this.label_bios);
             this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label_motherboard);
+            this.Controls.Add(this.textBox_motherboard);
+            this.Controls.Add(this.textBox_pc);
+            this.Controls.Add(this.label_pc);
+            this.Controls.Add(this.label_os);
+            this.Controls.Add(this.textBox_os);
+            this.Controls.Add(this.label_user);
+            this.Controls.Add(this.textBox_user);
+            this.Controls.Add(this.label_cores);
+            this.Controls.Add(this.textBox_cores);
+            this.Controls.Add(this.label_graphics);
+            this.Controls.Add(this.textBox_graphics);
+            this.Controls.Add(this.label_processor);
+            this.Controls.Add(this.textBox_processor);
+            this.MaximizeBox = false;
             this.Name = "Form1";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.Text = "System Info";
+            this.TopMost = true;
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -86,10 +225,22 @@
 
         #endregion
 
+        private System.Windows.Forms.TextBox textBox_processor;
+        private System.Windows.Forms.Label label_processor;
+        private System.Windows.Forms.TextBox textBox_graphics;
+        private System.Windows.Forms.Label label_graphics;
+        private System.Windows.Forms.TextBox textBox_cores;
+        private System.Windows.Forms.Label label_cores;
+        private System.Windows.Forms.TextBox textBox_user;
+        private System.Windows.Forms.Label label_user;
+        private System.Windows.Forms.TextBox textBox_os;
+        private System.Windows.Forms.Label label_os;
+        private System.Windows.Forms.Label label_pc;
+        private System.Windows.Forms.TextBox textBox_pc;
+        private System.Windows.Forms.TextBox textBox_motherboard;
+        private System.Windows.Forms.Label label_motherboard;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label_bios;
     }
 }
 
--- system info app/Form1.cs
+++ system info app/Form1.cs
@@ -18,9 +18,15 @@ namespace system_info_app
         public Form1()
         {
             InitializeComponent();
+
             // filling textboxes
             Processor();
             Graphic_Card();
+            textBox_user.Text = Environment.UserName;
+            textBox_os.Text = Environment.OSVersion.ToString();
+            textBox_pc.Text = Environment.MachineName.ToString();
+            textBox_motherboard.Text = MotherBoard();
+            textBox1.Text = Bios();
         }
 
         private void Processor()
@@ -31,10 +37,10 @@ namespace system_info_app
             {
                 if (processor_name.GetValue("ProcessorNameString") != null)
                 {
-                    textBox1.Text = processor_name.GetValue("ProcessorNameString").ToString();
+                    textBox_processor.Text = processor_name.GetValue("ProcessorNameString").ToString();
                 }
             }
-            //textBox1.Text = Environment.GetEnvironmentVariable("PROCESSOR_IDENTIFIER");
+            textBox_cores.Text = "кол-во ядер: " + Environment.ProcessorCount.ToString();
         }
         private void Graphic_Card()
         {
@@ -47,10 +53,40 @@ namespace system_info_app
                 {
                     if (property.Name == "Description")
                     {
-                        textBox2.Text = property.Value.ToString();
+                        textBox_graphics.Text = property.Value.ToString();
                     }
                 }
             }
+        }
+        private string MotherBoard()
+        {
+            ManagementObjectSearcher baseboardSearcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_BaseBoard");
+            ManagementObjectSearcher motherboardSearcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_MotherboardDevice");
+            try
+            {
+                foreach (ManagementObject queryObj in baseboardSearcher.Get())
+                {
+                    return queryObj["Product"].ToString();
+                }
+                return "";
+            }
+            catch (Exception e)
+            {
+                return "";
+            }
+        }
+        private string Bios()
+        {
+            ManagementObjectSearcher searcher1 = new ManagementObjectSearcher("SELECT * FROM Win32_BIOS");
+            ManagementObjectCollection collection = searcher1.Get();
+            foreach (ManagementObject obj in collection)
+            {
+                if (((string[])obj["BIOSVersion"]).Length > 1)
+                    return ("BIOS VERSION: " + ((string[])obj["BIOSVersion"])[0] + " - " + ((string[])obj["BIOSVersion"])[1]);
+                else
+                   return ("BIOS VERSION: " + ((string[])obj["BIOSVersion"])[0]);
+            }
+            return "";
         }
     }
 
